4ccd908408744f2f1035b9a023b7713342af2266
FaridAquino <farid.aquino@utec.edu.pe>
2025-05-13 16:42:11 -0500

    feat: add eventANDlisteners

---
+++ eventANDlisteners/src/main/java/com/example/eventANDlisteners/EventAnDlistenersApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventANDlisteners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAnDlistenersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventAnDlistenersApplication.class, args);
+	}
+
+}
---
+++ eventANDlisteners/src/test/java/com/example/eventANDlisteners/EventAnDlistenersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventANDlisteners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAnDlistenersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
