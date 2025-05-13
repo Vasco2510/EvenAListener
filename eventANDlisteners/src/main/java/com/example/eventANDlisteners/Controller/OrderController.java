b8811c95d7e7e683c32b30decec9c867e0fdcd24
stiffis <steve.ildefonso@utec.edu.pe>
2025-05-13 17:00:26 -0500

    feat: add ordercontroller

--- eventANDlisteners/src/main/java/com/example/eventANDlisteners/Controller/OrderController.java
+++ eventANDlisteners/src/main/java/com/example/eventANDlisteners/Controller/OrderController.java
@@ -1,5 +1,20 @@
 package com.example.eventANDlisteners.Controller;
 
+import com.example.eventANDlisteners.Entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
 public class OrderController {
-    
+
+    @Autowired private ApplicationEventPublisher eventPublisher;
+
+    @PostMapping
+    public String createOrder(@RequestBody Order order) {
+        eventPublisher.publishEvent(new OrderCreatedEvent(
+            order.getId(), order.getEmail(), order.getProducts()));
+        return "Order created successfully!";
+    }
 }
