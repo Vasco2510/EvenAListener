89e9a983a73d061a866356858d18336fb7b2f5e8
AntoMGB1 <marco.galindo.b@utec.edu.pe>
2025-05-13 16:57:56 -0500

    feat:compleate InventoryUpdateListener

--- eventANDlisteners/src/main/java/com/example/eventANDlisteners/Listener/InventoryUpdateListener.java
+++ eventANDlisteners/src/main/java/com/example/eventANDlisteners/Listener/InventoryUpdateListener.java
@@ -1,4 +1,18 @@
 package com.example.eventANDlisteners.Listener;
 
+import com.example.eventANDlisteners.Event.OrderCreatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class InventoryUpdateListener {
+
+    private static final Logger logger = LoggerFactory.getLogger(InventoryUpdateListener.class);
+
+    @EventListener
+    public void onOrderCreated(OrderCreatedEvent event) {
+        logger.info("Actualizando inventario para el producto: " + event.getProduct());
+    }
 }
